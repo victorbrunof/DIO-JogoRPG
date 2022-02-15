0f973692392437a0519702ddcc064416d28f0f28
Victor Bruno <victorbrunof@icloud.com>
2022-02-15 16:52:09 -0300

    Adicionar .gitignore e .gitattributes.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO_JogoRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
